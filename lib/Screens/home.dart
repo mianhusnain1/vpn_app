632ab44fb8faf2ba48a400e61997364bcc830e6b
mianhusnain1 <mianhusnain550@gmail.com>
2023-10-31 22:38:08 +0500

    Update

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -232,7 +232,7 @@ class _HomeState extends State<Home> {
                                                   
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [data(), data() ],
+                            children: [data(), data(), data() ],
                           ),
                         ],
                       ),
