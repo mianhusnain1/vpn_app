a2ad4a4fe11a8a20f32e02d1d806abe68ef3520b
mianhusnain1 <mianhusnain550@gmail.com>
2023-12-06 22:26:29 +0500

    Update

--- lib/Screens/api.dart
+++ lib/Screens/api.dart
@@ -11,14 +11,15 @@ final csvstring = response.body.split('#')[1].replaceAll('*', ' ');
 List<List<dynamic>> list = const CsvToListConverter().convert(csvstring);
 
 final header = list[0];
-final tempjson = {};
+final tempJson = {};
  // as the  0 index is the 1st list and the 1st list is always the header or title.
 for(int i = 0; i < header.length; i ++ ) {
-  tempjson.addAll({header[i]: list[1][i]});    // here i will mention all the enteries that I want to add in the tempjson variable. Json is always start and end at the {}
+  tempJson.addAll({header[i]: list[1][i]});    // here i will mention all the enteries that I want to add in the tempjson variable. Json is always start and end at the {}
 }
 
 // once the csv data is converted to the list there will be 1 header list and multiple data list in that list.
 // log(tempjson.toString());
-log(jsonEncode(tempjson));//  log(response.body) ;
+log(tempJson.toString());
+// log(jsonEncode(tempJson));//  log(response.body) ;
   }
 }
